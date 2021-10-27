0341a1ce825c2c33584d0397c07f176a127a52af
antoine-SAUVE <antoine.sauve@free.fr>
2021-10-27 11:35:00 +0200

    ajout script pour bdd aws

--- BACK/data/migrations/revert/init.sql
+++ BACK/data/migrations/revert/init.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-DROP TABLE IF EXISTS wall, user, column, element, participate;
+DROP TABLE IF EXISTS wall, "user", column, element, participate;
 DROP DOMAIN IF EXISTS pint, mail;
 
 COMMIT;

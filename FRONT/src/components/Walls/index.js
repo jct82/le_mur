50f2e805714df90161eec51898d256c327bdbe58
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-27 11:10:04 +0200

    titre du composant walls et bouton ajouter un mur

--- FRONT/src/components/Walls/index.js
+++ FRONT/src/components/Walls/index.js
@@ -1,7 +1,15 @@
 import './walls.scss';
+import Plus from 'src/assets/icons/plus-neg.png';
 
 const Walls = () => (
   <div className="projects">
+    <div className="projects__title">
+      <p>Mes Projets</p>
+    </div>
+    <div className="projects__addProjectBtn">
+      <p>ajouter un mur</p>
+      <img className="projects__addProjectBtn__plusIcon" src={Plus} alt="bouton ajouter un mur" />
+    </div>
   </div>
   // <ProjectList />
 );

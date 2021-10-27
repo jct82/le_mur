08f12aa2ac08ba1a68684857fa5e42d5903836f1
antoine-SAUVE <antoine.sauve@free.fr>
2021-10-27 17:39:57 +0200

    modifications .env pour test connexion distante

--- BACK/API-omur/app/controllers/wallController.js
+++ BACK/API-omur/app/controllers/wallController.js
@@ -3,10 +3,11 @@ const datamapper = require('../datamapper');
 const wallController = {
     test: async function (_, res){
 
-        const result = await datamapper.getAllUsers();
+        res.send('coucou')
+        // const result = await datamapper.getAllUsers();
 
-        console.log(result);
-        res.json(result.rows)
+        // console.log(result);
+        // res.json(result.rows)
         }
         // res.json(result.rows);
     

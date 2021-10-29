0c5bc8042ed88a29dbde93528ad8fa678ad68797
antoine-SAUVE <antoine.sauve@free.fr>
2021-10-29 09:00:35 +0200

    ajout dse cors

--- BACK/API-omur/app/router.js
+++ BACK/API-omur/app/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
+
+app.use(cors());
 
 // import controllers
 const wallController = require('./controllers/wallController');
